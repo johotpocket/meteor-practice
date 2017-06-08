1dfbe2871ac72a7df64a870edcf310b45460bb04
johotpocket <jo.hotpocket@gmail.com>
2017-06-08 15:28:25 -0600

    fixed fatal typo in tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { createContainer } from 'meteor/react-meteor-data'
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
 
-import { Tasks } from '../api/tasks.js'
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.jsx';
 
@@ -9,12 +9,6 @@ import Task from './Task.jsx';
 class App extends Component {
   renderTasks() {
     return this.props.tasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ))
-  }
-
-  renderTasks() {
-    return this.getTasks().map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
